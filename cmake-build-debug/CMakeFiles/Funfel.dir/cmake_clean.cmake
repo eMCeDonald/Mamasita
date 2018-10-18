137e3553f4bf54f9f5120a5cc49a4e9d1b49c4fb
Jakubgrebowicz <kiepa2gre@gmail.com>
2018-10-18 16:02:56 +0200

    sd
    pierwszy commit

---
+++ cmake-build-debug/CMakeFiles/Funfel.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Funfel.dir/src/functions.cpp.obj"
+  "CMakeFiles/Funfel.dir/src/main.cpp.obj"
+  "Funfel.pdb"
+  "Funfel.exe"
+  "Funfel.exe.manifest"
+  "libFunfel.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Funfel.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
